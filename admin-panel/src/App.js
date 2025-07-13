1855803addc8ebf8683369e4fabd78b0b93513d7
shruti-readwhere <shruti.jain@mediologysoftware.com>
2025-07-13 17:03:01 +0530

    gh pages

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -6,7 +6,7 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/telegram-bot">
       <Navbar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
